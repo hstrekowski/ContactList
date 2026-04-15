2753c8c18b96bdab9f476c33e05b5582b162226d
Hubert Strękowski <hstrekowski05@gmail.com>
2026-04-16 01:15:50 +0200

    test(domain) add unit tests for Category entity

---
+++ tests/ContactList.Domain.Tests/Entities/CategoryTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using ContactList.Domain.Entities;
+using ContactList.Domain.Exceptions;
+
+namespace ContactList.Domain.Tests.Entities
+{
+    public class CategoryTests
+    {
+        private class TestCategory : Category
+        {
+            public TestCategory() : base() { }
+        }
+
+        [Theory]
+        [InlineData("Work", "Work")]
+        [InlineData("  Family  ", "Family")]
+        [InlineData("Friends", "Friends")]
+        public void Constructor_WithValidName_ShouldCreateInstanceAndTrimName(string input, string expectedName)
+        {
+            // Arrange & Act
+            var category = new Category(input);
+
+            // Assert
+            category.Name.Should().Be(expectedName);
+            category.Subcategories.Should().NotBeNull().And.BeEmpty();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public void Constructor_WithEmptyName_ShouldThrowDomainException(string invalidName)
+        {
+            // Arrange & Act
+            Action act = () => new Category(invalidName);
+
+            // Assert
+            act.Should().Throw<DomainException>()
+                .WithMessage("Category name cannot be empty.");
+        }
+
+        [Fact]
+        public void ProtectedConstructor_ShouldInitializePropertiesWithDefaultValues()
+        {
+            // Arrange & Act
+            var category = new TestCategory();
+
+            // Assert
+            category.Name.Should().BeEmpty();
+            category.Subcategories.Should().NotBeNull().And.BeEmpty();
+        }
+    }
+}
