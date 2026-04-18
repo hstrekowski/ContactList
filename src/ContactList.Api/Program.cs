60f8448ac3ae4b22cc591fa6e6340368e9e4b9e0
Hubert Strękowski <hstrekowski05@gmail.com>
2026-04-18 17:42:20 +0200

    feat(api): basic configuration (db, jwt, packages)

--- src/ContactList.Api/Program.cs
+++ src/ContactList.Api/Program.cs
@@ -7,13 +7,8 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddControllers();
-        builder.Services.AddOpenApi();
+
         var app = builder.Build();
-       
-        if (app.Environment.IsDevelopment())
-        {
-            app.MapOpenApi();
-        }
 
         app.UseHttpsRedirection();
         app.UseAuthorization();
